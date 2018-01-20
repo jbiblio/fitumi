c8ac8ecc293d16b91c77da11b3f98aaf9d6fa23b
Michael Holtermann <michael@jbiblio.de>
2018-01-20 22:21:08 +0100

    Finish twitter registration. Fetch user's profile, create or update existing user and saves it into db.

--- src/main/java/com/acme/fitumi/config/CloudConfig.java
+++ src/main/java/com/acme/fitumi/config/CloudConfig.java
@@ -5,10 +5,11 @@ import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ServiceScan
-// @Profile("cloud")
+@Profile("cloud")
 public class CloudConfig extends AbstractCloudConfig {
 	@Bean
 	public ApplicationInstanceInfo applicationInfo() {
