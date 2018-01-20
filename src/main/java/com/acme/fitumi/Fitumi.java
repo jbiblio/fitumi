ce2c64c62206768d88b7e84423d2cc95e2b5882c
Michael Holtermann <michael.holtermann@dfb-medien.de>
2018-01-20 13:02:36 +0100

    Basic Twitter Login Page

---
+++ src/main/java/com/acme/fitumi/Fitumi.java
@@ -0,0 +1,12 @@
+package com.acme.fitumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fitumi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fitumi.class, args);
+	}
+}
